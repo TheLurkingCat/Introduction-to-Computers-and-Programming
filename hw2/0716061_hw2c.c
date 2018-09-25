a12c7210fa0658b18a485bfd4c304bb6e117c337
Meow <james.liu841@gmail.com>
2018-09-25 13:08:33 +0800

    第三版

--- hw2/0716061_hw2c.c
+++ hw2/0716061_hw2c.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    long long day, month, year;
+    int day, month, year;
     printf("Please enter a data (format: mm/dd/yyyy): ");
-    scanf("%lld/%lld/%lld", &month, &day, &year);
-    printf("your input data is %04lld%02lld%02lld.\n", year, month, day);
+    scanf("%d/%d/%d", &month, &day, &year);
+    printf("your input data is %04d%02d%02d.\n", year, month, day);
     return 0;
 }
--- hw2/0716061_hw2e.c
+++ hw2/0716061_hw2e.c
@@ -1,8 +1,8 @@
+#include <ctype.h>
 #include <stdio.h>
 
 int main(void) {
     printf("Please enter a char: ");
-    int c = getchar();
-    printf("The uppercase char is %c.\n", (c <= 122 && c >= 97) ? c - 32 : c);
+    printf("The uppercase char is %c.\n", toupper(getchar()));
     return 0;
 }
