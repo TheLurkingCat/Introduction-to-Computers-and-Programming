5a5cf43f11ba197a8970565305f392aa06eeb739
Meow <james.liu841@gmail.com>
2018-09-26 15:05:24 +0800

    最終版

--- hw2/0716061_hw2a.c
+++ hw2/0716061_hw2a.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("char's memory size: %d byte\n", sizeof(char));
-    printf("short's memory size: %d bytes\n", sizeof(short));
-    printf("int's memory size: %d bytes\n", sizeof(int));
-    printf("float's memory size: %d bytes\n", sizeof(float));
-    printf("double's memory size: %d bytes\n", sizeof(double));
+    printf("char's memory size: %d byte%c\n", sizeof(char),
+           's' * (sizeof(char) - 1));
+    printf("short's memory size: %d bytes\n", sizeof(short),
+           's' * (sizeof(short) - 1));
+    printf("int's memory size: %d bytes\n", sizeof(int), 's' * (sizeof(int) - 1));
+    printf("float's memory size: %d bytes\n", sizeof(float),
+           's' * (sizeof(float) - 1));
+    printf("double's memory size: %d bytes\n", sizeof(double),
+           's' * (sizeof(double) - 1));
     return 0;
 }
--- hw2/0716061_hw2f.c
+++ hw2/0716061_hw2f.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-    long long number;
-    printf("Please enter a number: ");
-    while (scanf("%lld", &number) && number != -1) {
+    double number;
+    while (printf("Please enter a number: ") && scanf("%lf", &number) &&
+           number != -1) {
         printf("The number is ");
-        printf(number & 1 ? "odd" : "even");
-        printf(".\nPlease enter a number: ");
+        printf((int)number & 1 ? "odd" : "even");
+        printf(".\n");
     }
     return 0;
 }
