9f61b268ac916f35d0bc2b401fe85edd36880cc0
Meow <james.liu841@gmail.com>
2018-11-01 19:48:08 +0800

    第四版

--- hw6/0716061_hw6f.c
+++ hw6/0716061_hw6f.c
@@ -2,53 +2,39 @@
 
 #define min(X, Y) (X) < (Y) ? (X) : (Y)
 #define max(X, Y) (X) > (Y) ? (X) : (Y)
-struct minmax {
-    int max, min;
-};
 
 int maxProfit(int* prices, int pricesSize) {
-    /* O(N^2) */
-    struct minmax profits[100][100];
-    int i, j, left, right, ans = 0, last_min, last_max;
-    if (pricesSize == 1)
-        return 0;
-    if (pricesSize == 2)
-        return prices[1] > prices[0] ? prices[1] - prices[0] : 0;
-    if (pricesSize == 3) {
-        for (i = 0; i < pricesSize - 1; i++)
-            for (j = i; j < pricesSize; j++)
-                ans = max((prices[j] - prices[i]), ans);
-        return ans;
+    /* O(N) */
+    int arr[100], arr_rev[100], i, j, k, min_value, max_value, ans;
+
+    k = pricesSize - 1;
+    min_value = prices[0];
+    max_value = prices[k];
+    arr_rev[k] = arr[0] = 0;
+
+    for (i = 1; i < pricesSize; i++) {
+        j = pricesSize - i;
+
+        arr[i] = max(arr[i - 1], prices[i] - min_value);
+        arr_rev[j - 1] = max(arr_rev[j], max_value - prices[j - 1]);
+
+        min_value = min(min_value, prices[i]);
+        max_value = max(max_value, prices[j - 1]);
     }
-    for (i = 0; i < pricesSize; i++) {
-        last_min = 2147483647, last_max = -2147483647;
-        for (j = i; j < pricesSize; j++) {
-            last_max = max(last_max, prices[j]);
-            last_min = min(last_min, prices[j]);
-            profits[i][j].min = last_min;
-            profits[i][j].max = last_max;
-        }
-    }
-    for (i = 0; i < pricesSize; i++) {
-        left = right = 0;
-        for (j = 1; j < i; j++) {
-            left = max(profits[j][i - 1].max - profits[0][j - 1].min, left);
-        }
-        for (j = i + 1; j < pricesSize; j++) {
-            right = max(profits[j][pricesSize - 1].max - profits[i][j - 1].min, right);
-        }
-        ans = max(ans, right + left);
-    }
+
+    ans = arr[k];
+
+    for (i = 0; i < k; i++)
+        ans = max(ans, arr[i] + arr_rev[i + 1]);
 
     return ans;
 }
 
 int main() {
-    int pricesSize;
-    int prices[100];
+    int pricesSize, prices[100], i;
 
     scanf("%d", &pricesSize);
-    for (int i = 0; i < pricesSize; i++) {
+    for (i = 0; i < pricesSize; i++) {
         scanf("%d", &prices[i]);
     }
     printf("%d", maxProfit(prices, pricesSize));
